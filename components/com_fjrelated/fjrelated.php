e2331f7737752d4793ddfaf27530046aa1d7cf40
Mark Dexter <dextercowley@gmail.com>
2013-08-22 14:13:01 -0700

    Initial commit for com_fjrelated

---
+++ components/com_fjrelated/fjrelated.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @version		$Id: fjrelated.php 99 2011-05-03 22:49:23Z dextercowley $
+ * @package		com_fjrelated_plus
+ * @copyright	Copyright (C) 2008 Mark Dexter. Portions Copyright Open Source Matters. All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+// no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Require the base controller
+
+jimport('joomla.application.component.controller');
+
+// Create the controller
+$controller = JControllerLegacy::getInstance('FJRelated');
+$controller->execute(JRequest::getCmd('task'));
+$controller->redirect();
+
+?>
